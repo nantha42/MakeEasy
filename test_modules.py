a67ecfd5a47d4f2925ae38a4577ccf64179d4f99
nantha42 <krnantha13@gmail.com>
2021-10-08 21:55:16 +0530

    now functionalble

--- test_modules.py
+++ test_modules.py
@@ -1,4 +1,12 @@
+import json
 from src.modules import *
+
+with open(".password.json","r") as f:
+    obj = json.load(f)
+    user = obj["user"]
+    password = obj["password"]
+    project_name = obj["project_name"]
+    db_name = obj["db_name"]
 
 
 def test_add_pays():
@@ -8,7 +16,8 @@ def test_add_pays():
     app.add_past_pay(customer_id=2,debit_id=1,amount=200,time_str="2021:6:1")
     app.add_past_pay(customer_id=2,debit_id=1,amount=200,time_str="2021:7:1")
     app.add_pay(customer_id=2, debit_id=1, amount=500)
-    principal = app.get_debit_principal(2,1 )
+    principal = str(app.get_debit_principal(2,1))[:7]
+    assert principal ==  "255.512"
 
 if __name__ == '__main__':
     test_add_pays()
