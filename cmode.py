ffdb7383286aefdda503b2b5a98ad5a57a8edd72
nantha42 <krnantha13@gmail.com>
2021-10-20 17:31:04 +0530

    added customer creation and customer show

--- cmode.py
+++ cmode.py
@@ -1,12 +1,14 @@
 from src.modules import *
+from pprint import pprint
 
 
 class CMode:
-    def __init__(self):
+    def __init__(self,mode):
         password = ".password.json"
-        self.options = ["create user","create debit","delete user","delete debit","quit"]
-        # self.app = App(password)
+        self.options = ["create customer","show customers","create debit","delete user","delete debit","quit"]
+        self.app = App(password)
         self.quit = False
+        self.mode =mode
 
     def print_options(self):
         opt_indexes = {}
@@ -14,6 +16,33 @@ class CMode:
             print(f"{i}: {opt}")
             opt_indexes[i] = opt
         return opt_indexes
+
+    def act(self,opt):
+        if opt == "create customer":
+            name = input("Customer Name: ")
+            mobile = input("Customer mobile: ")
+            doorno = input("Customer doorno: ")
+            street = input("Customer street: ")
+            self.app.add_user(name,
+                              mobile,
+                              {"doorno":doorno,
+                               "street":street},
+                              self.mode)
+        if opt == "create debit":
+
+            pass
+
+        if opt == "show customers":
+            count = self.app.get_users_count()
+            for id in range(1,count+1):
+                cus = self.app.get_user(id,self.mode)
+                print(f"{id:<3} : {cus['name']:20s}")
+            print()
+
+
+
+
+
 
     def run(self):
         while not self.quit:
@@ -23,11 +52,13 @@ class CMode:
                 choosen_opt = opt_indexes[inp]
                 if choosen_opt == "quit":
                     self.quit = True
+                self.act(choosen_opt)
 
             except KeyError:
                 print("Not a valid option,")
                 pass
 
 if __name__ == '__main__':
-    cm = CMode()
+    mode = "production"
+    cm = CMode(mode)
     cm.run()
